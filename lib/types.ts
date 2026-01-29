fbb213186a0adc2a0f7e7f87019bcac84abf3377
derhderhderh <160904655+derhderhderh@users.noreply.github.com>
2026-01-29 14:10:10 -0600

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -1,7 +1,7 @@
 export interface User {
   uid: string
   email: string
-  displayName?: string
+  displayName: string | null
   role: "user" | "admin"
   purchasedCourses: string[]
   createdAt: Date
